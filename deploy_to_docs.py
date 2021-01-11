9b0447c213524503d29a5e956504dbc3e6a78760
Justin Golden <justingolden21@gmail.com>
2021-01-11 14:16:47 -0800

    Create deploy_to_docs.py
    source:
    https://stackoverflow.com/a/53053105/4907950

---
+++ deploy_to_docs.py
@@ -0,0 +1,16 @@
+import os, shutil
+
+def recursive_copy(src, dest):
+	for item in os.listdir(src):
+		file_path = os.path.join(src, item)
+
+		if os.path.isfile(file_path):
+			shutil.copy(file_path, dest)
+
+		elif os.path.isdir(file_path):
+			new_dest = os.path.join(dest, item)
+			os.mkdir(new_dest)
+			recursive_copy(file_path, new_dest)
+
+recursive_copy('dist/', 'docs/')
+input('Done.')
