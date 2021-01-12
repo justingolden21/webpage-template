b99dd9a83765f1fe1ba6a6614e1d0d247ca6a070
Justin Golden <justingolden21@gmail.com>
2021-01-11 16:30:59 -0800

    fix manifest img links

--- tailwind_pwa_boilerplate.py
+++ tailwind_pwa_boilerplate.py
@@ -140,32 +140,32 @@ manifest_json_text = """{
 	"orientation": "portrait-primary",
 	"icons": [
 		{
-			"src": "/img/icons/icon-48x48.png",
+			"src": "img/icons/icon-48x48.png",
 			"type": "image/png",
 			"sizes": "48x48"
 		},
 		{
-			"src": "/img/icons/icon-72x72.png",
+			"src": "img/icons/icon-72x72.png",
 			"type": "image/png",
 			"sizes": "72x72"
 		},
 		{
-			"src": "/img/icons/icon-96x96.png",
+			"src": "img/icons/icon-96x96.png",
 			"type": "image/png",
 			"sizes": "96x96"
 		},
 		{
-			"src": "/img/icons/icon-144x144.png",
+			"src": "img/icons/icon-144x144.png",
 			"type": "image/png",
 			"sizes": "144x144"
 		},
 		{
-			"src": "/img/icons/icon-192x192.png",
+			"src": "img/icons/icon-192x192.png",
 			"type": "image/png",
 			"sizes": "192x192"
 		},
 		{
-			"src": "/img/icons/icon-512x512.png",
+			"src": "img/icons/icon-512x512.png",
 			"type": "image/png",
 			"sizes": "512x512"
 		}
